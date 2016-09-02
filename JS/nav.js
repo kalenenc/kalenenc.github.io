42fd2ae0f9cbfa41e8b316940d917eb6d48e6464
kalenenc <kalenenc@gmail.com>
2016-09-02 16:27:46 -0400

    made a separate JS file

---
+++ JS/nav.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+
+
+// Navigation
+
+$('.nav-icon').on('click', function() {
+  this.classList.toggle('active');
+});
+
+
+$(".nav-icon").on("click", function(){
+	$('nav ul').slideToggle();
+});
+
+
+
+
+});
+
+
+
+// Functions
