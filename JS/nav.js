16fa3b8e4031ce6dc40f73fa062eff3a9eb9799e
kalenenc <kalenenc@gmail.com>
2016-09-02 22:10:56 -0400

    added some animations

--- JS/nav.js
+++ JS/nav.js
@@ -13,11 +13,34 @@ $(".nav-icon").on("click", function(){
 	$('nav ul').slideToggle();
 });
 
+// Function Calls
 
+$(function(){
+	beeJump();
+  	setInterval(function(){ beeJump(); }, 5000);
+    // setInterval(function(){ callTada(); }, 1000);
+    setInterval(function(){ movePencil(); }, 3000);
+});
+
+
+
+// Defining Functions
+
+function beeJump() {
+  $(".header svg").toggleClass("bee-jump");
+}
+
+// function callTada() {
+// 	var randNum = Math.floor(Math.random() * $(".section2 i").length) + 1;
+// 	$(".section2 i").eq(randNum).addClass("tada").siblings().removeClass("tada");
+// }
+
+function movePencil() {
+	$(".pencil").toggleClass("movePencil");
+}
 
 
 });
 
 
 
-// Functions
